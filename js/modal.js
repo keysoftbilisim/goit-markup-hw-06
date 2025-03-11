43e4f410bf9758aab449828067ee41899e730b9a
ilhan <keysoftbilisimsistemleri@gmail.com>
2025-03-12 00:24:50 +0300

    update

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,18 @@
-( () => {
+(() => {
   const refs = {
+   
     openModalBtn: document.querySelector("[data-modal-open]"),
+    
     closeModalBtn: document.querySelector("[data-modal-close]"),
+    
     modal: document.querySelector("[data-modal]"),
   };
-  if (refs.openModalBtn && refs.closeModalBtn) {
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  }
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
   function toggleModal() {
+    
     refs.modal.classList.toggle("is-open");
   }
-} )();efs.modal.classList.toggle("is-open");
-  
-();
+})();
